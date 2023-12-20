39c39de8545c6baed9b3ad9dc9344025d459582b
Joe <jakatsa5@gmail.com>
2023-12-20 21:43:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards() {
+  let thankYouMessage = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    thankYouMessage.push(message);
+  }
+  return thankYouMessage;
+}
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+console.log(writeCards(names));
+/***** */
+let num = 11;
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+console.log(countDown(num));
